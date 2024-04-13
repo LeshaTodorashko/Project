33237d33e0b39f5cd0ff204fa5c47aa8545f4480
Yarikwds <oftanksworld55@gmail.com>
2024-04-13 13:15:29 +0300

    idk

--- Meme Generator.py
+++ Meme Generator.py
@@ -1,25 +1,17 @@
 import pygame
 import random
-
-# Ініціалізація Pygame
 pygame.init()
-
-# Розміри вікна
 WIDTH, HEIGHT = 800, 600
-
-# Колір фону
 WHITE = (255, 255, 255)
-
-# Створення вікна
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Мем Вьюер")
 
-# Список категорій та зображень для кожної категорії
+
 categories = ["Категорія 1", "Категорія 2", "Категорія 3"]
 images = {
-    "Категорія 1": [pygame.image.load("image1.jpg"), pygame.image.load("image2.jpg"), pygame.image.load("image3.jpg")],
-    "Категорія 2": [pygame.image.load("image4.jpg"), pygame.image.load("image5.jpg"), pygame.image.load("image6.jpg")],
-    "Категорія 3": [pygame.image.load("image7.jpg"), pygame.image.load("image8.jpg"), pygame.image.load("image9.jpg")]
+    "Категорія 1": [pygame.image.load("meme1.jpg"), pygame.image.load("meme2.jpg"), pygame.image.load("meme3.jpg")],
+    "Категорія 2": [pygame.image.load("meme4.jpg"), pygame.image.load("meme5.jpg"), pygame.image.load("meme6.jpg")],
+    "Категорія 3": [pygame.image.load("meme7.jpg"), pygame.image.load("meme8.jpg"), pygame.image.load("meme9.jpg")]
 }
 
 selected_image = None
@@ -34,23 +26,23 @@ def draw_buttons():
     button_y = 50
     for category in categories:
         button_rect = pygame.Rect(50, button_y, 200, 30)
-        pygame.draw.rect(screen, (0, 0, 255), button_rect)
+        pygame.draw.rect(window, (0, 0, 255), button_rect)
         font = pygame.font.Font(None, 24)
         text = font.render(category, True, WHITE)
-        screen.blit(text, (button_rect.x + 10, button_rect.y + 5))
+        window.blit(text, (button_rect.x + 10, button_rect.y + 5))
         button_y += 40
 
 # Головний цикл програми
-running = True
-while running:
-    screen.fill(WHITE)
+game = True
+while game:
+    window.fill(WHITE)
 
     draw_buttons()
 
     # Обробка подій
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            game = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -62,8 +54,8 @@ while running:
 
     # Відображення вибраного зображення
     if selected_image:
-        screen.blit(selected_image, (0, 0))
+        screen.blit(selected_image, (350, 200))
 
-    pygame.display.flip()  # Оновлення екрану
+    pygame.display.update()  # Оновлення екрану
 
 pygame.quit()
